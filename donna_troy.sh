4863ed44f9d56ffcde3c4cd3165717ae88403993
root <anderson.bianchini@teste.com>
2019-04-18 01:23:25 +0000

    Add da variavel IP no echo

--- donna_troy.sh
+++ donna_troy.sh
@@ -2,5 +2,5 @@
 
 for IP in 10 20 30 40
 do
-	ping -c 2 192.168.99.$IP &> /dev/null && echo "SHAZAM" || echo "BLAM!"
+	ping -c 2 192.168.99.$IP &> /dev/null && echo "SHAZAM" || echo "$IP BLAM!"
 done
